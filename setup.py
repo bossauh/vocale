9d873ef60ddf50031f3db6190adb426222c2a19e
matt <philmattdev@gmail.com>
2022-01-15 00:48:19 +0800

    version update

--- setup.py
+++ setup.py
@@ -16,13 +16,13 @@ from distutils.core import setup
 setup(
     name="vocale-python",
     packages=["vocale"],
-    version="0.4",
+    version="0.5",
     license="MIT",
     description="A speech library designed specifically for personal assistants. It features a wake word recognizer, voice activity detection, offline mode, and a speech synthesizer.",
     author="Philippe Mathew",
     author_email="philmattdev@gmail.com",
     url="https://github.com/bossauh/vocale",
-    download_url="https://github.com/bossauh/vocale/releases/tag/v_04",
+    download_url="https://github.com/bossauh/vocale/releases/tag/v_05",
     keywords=["speech", "recognition"],
     install_requires=install_requires,
     classifiers=[
